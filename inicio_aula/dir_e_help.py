d8163400c50c0ece782ec2cbc9634caf4063d565
vitor <vitor.scastro2013@hotmail.com>
2022-12-12 18:42:50 -0300

    Continuação da aula

---
+++ inicio_aula/dir_e_help.py
@@ -0,0 +1,16 @@
+"""
+Utilitarios python para te auxiliar na programação.
+
+dir: Apresenta todos os atributos a funções/métodos disponiveis para  determinado tipo de dado ou variavel.
+
+dir(tipo dado/variavel)
+print(dir("Geek"))
+print(("Geek").lower())
+
+help: Apresenta a documentação/como utilizar os atributos/propriedades e funções/metodos disponiveis para
+determinada tipo de dado ou variavel.
+
+"""
+
+#print(help(dir("Geek"))
+help("Geek".upper())
