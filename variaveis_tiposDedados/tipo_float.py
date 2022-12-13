43b1b5609dae59a68da840c8d8dfad0eb8ba9cb2
vitor <vitor.scastro2013@hotmail.com>
2022-12-13 15:20:18 -0300

    Enviando novas atualizações

---
+++ variaveis_tiposDedados/tipo_float.py
@@ -0,0 +1,25 @@
+"""
+Tipo float ou tipo real ou tipo decimal
+
+Como a linguagem é baseada no idioma ingles, o separador de casas é ponto e nao virgula, exemplo :
+5.5 e nao 5,5
+"""
+#errado
+valor = 1,33
+
+print(valor)
+
+#certo
+
+valor2 = 1.33
+
+print(valor2)
+
+valor3, valor4 = 1, 44 # é possovel fazer dupla atribuição de valor
+print(valor3, valor4)
+print(valor3)
+print(valor4)
+
+
+
+
---
+++ variaveis_tiposDedados/tipo_numerico.py
@@ -0,0 +1,17 @@
+"""
+Tipo Numérico
+
+
+"""
+print(5 / 2) # divisao normal
+print(5 // 2) # modo pythonico de rtornar um numero inteiro
+print(4 % 2) # resto da divisao, resto 0 é numero par
+print(5 % 2) # resto da divisao, resto 1 numero impar
+print(2 ** 4) # numero elevado a outro
+print(1_000_000) # da para seperar por _ para saber o numero
+
+
+print(type(34)) # diz o tipo do dado, no caso int
+
+num = 40
+print(num.__add__(8)) # __add__ funciona para adicionar a uma variavel
