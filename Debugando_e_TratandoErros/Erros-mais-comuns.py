d74289aca565e77859be5b2616ed8d9c2a6b7bb6
vitor <vitor.scastro2013@hotmail.com>
2023-02-01 18:22:40 -0300

    Nova aula

--- Debugando_e_TratandoErros/Erros-mais-comuns.py
+++ Debugando_e_TratandoErros/Erros-mais-comuns.py
@@ -1,4 +1,45 @@
 """
 Erros mais comuns
 
+# ATENÇAO !!!
+É importante prestar atenção e aprender a ler as saidas de erro geradas pela execução do nosso codigo.
+
+
+OS erros mais comuns:
+
+1 - SyntaxError -> ocorre quando o python encontra algum erro de sintaxe, ou seja, voce escreveu algo que o python
+nao reconhece como parte da linguagem.
+
+# Exemplos SyntaxError
+
+a)
+
+def funcao:
+    print('Geek')
+
+b)
+
+None = 1
+def = 2
+
+2 - NameError -> ocorre quando uma variavel ou funçao nao foi definida
+
+# Exemplos NameError
+
+a)
+
+print(geek)
+
+b)
+
+geek()
+
 """
+
+
+# Exemplos NameError
+
+geek()
+
+
+
