2c0791e4a83cf6932be9c3eff5ad0ed7af59bcda
vitor <vitor.scastro2013@hotmail.com>
2023-02-06 18:07:06 -0300

    Novas aulas

---
+++ Modulos_e_Pacotes/Dunder.py
@@ -0,0 +1,28 @@
+"""
+Dunder Name e Dunder Main
+
+Dunder -> Double Under
+
+Dunder Name -> __name__
+
+Dunder Main -> __main__
+
+Em Python, sao utilizados dunder para criar funções, atributos, propriedades e etc utilizando Double Under,
+para nao gerar conflito com os nomes desses elementos na programação
+
+# Na linguagem C, temos um programa da seguinte forma:
+
+int main(){
+    return 0;
+}
+
+# Na linguagem Java temos um programa da seguinte forma:
+
+public static void main(String[] args){
+
+}
+
+# Em Python, se executarmos um modulo python diretamente na linha de comando, internamente o python
+atribuirá a variavel __name__ o valor __main__ indicando que esse modulo é o modulo de execução principal.
+"""
+
---
+++ Modulos_e_Pacotes/Pacotes.py
@@ -0,0 +1,16 @@
+"""
+Pacotes
+
+Modulo -> É apenas um arquivo python, que pode ter diversas funções para utilizarmos.
+
+Pacotes -> É um diretório que contém uma coleçõa de módulos.
+
+OBS : Nas versoes 2.x do Python, um pacote python deveria conter dentro dele um arquivo chamado __init__.py
+
+Nas versoes do python 3.x, nao é mais obrigatória a utilização desse arquivo, mas normalmente ainda
+é utilizado para manter compatibilidade.
+
+from Funcoes import funcoes_com_parametro as fcp # pacote Funcoes, modulo funcoes_com_parametro
+
+print(fcp.soma_impares([1,2,3,4,5]))
+"""
