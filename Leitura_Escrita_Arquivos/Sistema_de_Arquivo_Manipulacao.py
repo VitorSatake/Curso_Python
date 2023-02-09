6d0aa4b251ce18d98a4a952bf9203dd23cf6288f
vitor <vitor.scastro2013@hotmail.com>
2023-02-09 17:29:05 -0300

    Nova aula

---
+++ Leitura_Escrita_Arquivos/Sistema_de_Arquivo_Manipulacao.py
@@ -0,0 +1,22 @@
+"""
+Sistema de Arquivos - Manipulação
+
+# Descobrindo se um arquivo ou diretório existe
+
+# Arquivo
+print(os.path.exists('university.txt'))
+
+# Diretório
+
+# Paths Relativos
+print(os.path.exists('colecoes'))
+# Diretorio/Arquivo
+print(os.path.exists('colecoes/Conjuntos.py'))
+
+# Paths Absolutos
+print(os.path.exists('D:\CURSO_PYTHON_GEEK'))
+print(os.path.exists('D:\CURSO_PYTHON_GEEK\Collections'))
+"""
+import os 
+
+
