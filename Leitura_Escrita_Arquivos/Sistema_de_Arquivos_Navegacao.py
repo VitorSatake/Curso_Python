993b816f8974f688c3971d8f83be97d7f074b637
vitor <vitor.scastro2013@hotmail.com>
2023-02-09 16:03:58 -0300

    Nova aula

--- Leitura_Escrita_Arquivos/Sistema_de_Arquivos_Navegacao.py
+++ Leitura_Escrita_Arquivos/Sistema_de_Arquivos_Navegacao.py
@@ -19,5 +19,39 @@ print(os.getcwd())
 
 os.chdir('..')
 print(os.getcwd())
+
+# Podemos checar ssse um diretorio é absoluto ou relativo
+
+print(os.path.isabs('D:\\Curso_Python_Geek'))
+
+# OBS para usuarios Windows:
+
+# Se você tiver utilizando um computador com Windows, deverá ter cuidado para verificar diretórios !!!
+
+
+# Podemos identificar o sistema operacional com o modulo os
+
+#print(os.name) # posix (linux e mac) , nt (Windows)
+
+# Podemos ter mais detalhes do sistema operacional
+
+#print(os.uname()) # para linux
+
+import sysconfig
+
+print(sysconfig)
+
+print(sysconfig.get_platform())
+
+import sys
+
+print(sys.platform)
+
+print(sys.version)
+
+print(sys.executable)
 """
+# Fazer o import
+
+import os
 
