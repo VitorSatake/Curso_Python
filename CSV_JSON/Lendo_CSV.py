fd83b5128c9175565b2a0d83a1272d867c8d1a40
vitor <vitor.scastro2013@hotmail.com>
2023-03-02 16:56:53 -0300

    Nova aula

--- CSV_JSON/Lendo_CSV.py
+++ CSV_JSON/Lendo_CSV.py
@@ -21,10 +21,49 @@ CSV - Comma Separate Values - Valores separados por virgula
 Site para conseguir dados:
 http://dados.gov.br/dataset
 
-"""
-
+# Possivel de se trabalhar, mas nao éo ideal (trabalhoso)
 with open('lutadores.csv') as arquivo:
     dados = arquivo.read()
-    print(type(dados))
+    #print(type(dados))
+    dados = dados.split(',')[2]
     print(dados)
 
+    
+A linguagem pythonpossui duas formas diferentes para ler dados em arquivos csv:
+    - reader -> permite que iteremos sobre as linhas do arquivo csv como listas;
+    - DictReader -> permite que iteremos sobre as linhas do arquivo csv como OrderedDicts;
+
+
+# Reader
+
+from csv import reader
+
+with open("lutadores.csv") as arquivo:
+    leitor_csv = reader(arquivo)
+    next(leitor_csv)
+    for linha in leitor_csv:
+        # cada linha é uma lista
+        print(f'{linha[0]} nasceu no(a)(s) {linha[1]} e mede {linha[2]} centimetros.')
+
+
+# DictReader
+
+from csv import DictReader
+
+with open('lutadores.csv') as arquivo:
+    leitor_csv = DictReader(arquivo)
+    for linha in leitor_csv:
+        # cada linha é um OrderedDict
+        print(f"{linha['Nome']} nasceu no(a)(s) {linha['País']} e mede {linha['Altura (em cm)']} centimetros.")
+"""
+
+
+# DictReader com outro separador
+
+from csv import DictReader
+
+with open('lutadores.csv') as arquivo:
+    leitor_csv = DictReader(arquivo, delimiter=',')
+    for linha in leitor_csv:
+        # cada linha é um OrderedDict
+        print(f"{linha['Nome']} nasceu no(a)(s) {linha['País']} e mede {linha['Altura (em cm)']} centimetros.")
